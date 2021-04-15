fe3fdec60f77b726425a01d1654b5afe1ee362ee
yskszk63 <yskszk63@gmail.com>
2021-04-15 23:18:36 +0900

    check flake8

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from typing import Optional
 import hmac
+from urllib.parse import urlencode
 
 from fastapi import FastAPI, Header, Request, HTTPException
 from fastapi.responses import RedirectResponse
@@ -12,6 +13,8 @@ CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
 WEBHOOK_SECRET = os.environ['WEBHOOK_SECRET']
 DATABASE_URL = os.environ['DATABASE_URL']
+TOKEN_ENDPOINT = os.environ.get('TOKEN_ENDPOINT',
+                                'https://github.com/login/oauth/access_token')
 
 db = None
 
@@ -32,9 +35,20 @@ class Database:
         pool = await asyncpg.create_pool(url, min_size=1, max_size=1)
 
         try:
-            await pool.execute(
-                    "CREATE TABLE IF NOT EXISTS token (id INT, access_token TEXT, expires_in INT, refresh_token TEXT, refresh_token_expires_in INT, token_type TEXT, primary key (id))")
-        except:
+            sql = """
+                CREATE TABLE IF NOT EXISTS token (
+                    id INT NOT NULL,
+                    access_token TEXT NOT NULL,
+                    expires_in INT NOT NULL,
+                    refresh_token TEXT NOT NULL,
+                    refresh_token_expires_in TEXT NOT NULL,
+                    token_type TEXT NOT NULL,
+                    primary key (id)
+                )
+            """
+            await pool.execute(sql)
+
+        except Exception:
             await pool.close()
             raise
 
@@ -51,22 +65,51 @@ class Database:
             connection: asyncpg.Connection
 
             async with connection.transaction():
-                await connection.execute(
-                        "INSERT INTO token(id, access_token, expires_in, refresh_token, refresh_token_expires_in, token_type) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT ON CONSTRAINT token_pkey DO UPDATE SET access_token=$2, expires_in=$3, refresh_token=$4, refresh_token_expires_in=$5, token_type=$6",
-                        installation_id,
-                        token.access_token,
-                        token.expires_in,
-                        token.refresh_token,
-                        token.refresh_token_expires_in,
-                        token.token_type)
+                sql = """
+                    INSERT INTO token (
+                        id,
+                        access_token,
+                        expires_in,
+                        refresh_token,
+                        refresh_token_expires_in,
+                        token_type
+                    ) VALUES ($1, $2, $3, $4, $5, $6)
+                    ON CONFLICT ON CONSTRAINT token_pkey DO
+                    UPDATE SET
+                        access_token = $2,
+                        expires_in = $3,
+                        refresh_token = $4,
+                        refresh_token_expires_in = $5,
+                        token_type = $6
+                """
+
+                await connection.execute(sql,
+                                         installation_id,
+                                         token.access_token,
+                                         token.expires_in,
+                                         token.refresh_token,
+                                         token.refresh_token_expires_in,
+                                         token.token_type)
 
     async def get_token(self, installation_id: int) -> Token:
         async with self.pool.acquire() as connection:
             connection: asyncpg.Connection
 
-            row = await connection.fetchrow(
-                    "SELECT id, access_token, expires_in, refresh_token, refresh_token_expires_in, token_type FROM token WHERE id=$1",
-                    installation_id)
+            sql = """
+                SELECT
+                    id,
+                    access_token,
+                    expires_in,
+                    refresh_token,
+                    refresh_token_expires_in,
+                    token_type
+                FROM
+                    token
+                WHERE
+                    id = $1
+            """
+
+            row = await connection.fetchrow(sql, installation_id)
             if row is not None:
                 return Token(**row)
         raise Exception("not found.")
@@ -118,13 +161,15 @@ async def on_startup():
     global db
     db = await Database.open(DATABASE_URL)
 
+
 @app.on_event("shutdown")
 async def on_shutdown():
     await db.close()
 
+
 # ?code=9e7991b2cf5366cd5e93&installation_id=16228562&setup_action=install
 @app.get("/oauth2/callback")
-async def oauth2_callback(code: str, installation_id: int, setup_action: str):
+async def oauth2_callback(code: str, installation_id: int):
     async with AsyncClient() as client:
         data = {
             'client_id': CLIENT_ID,
@@ -134,7 +179,9 @@ async def oauth2_callback(code: str, installation_id: int, setup_action: str):
         headers = {
             'Accept': 'application/json',
         }
-        response = await client.post('https://github.com/login/oauth/access_token', data=data, headers=headers)
+        response = await client.post(TOKEN_ENDPOINT,
+                                     data=data,
+                                     headers=headers)
         if response.is_error:
             raise HTTPException(response.status_code, response.text)
 
@@ -142,10 +189,13 @@ async def oauth2_callback(code: str, installation_id: int, setup_action: str):
         token = Token(**token)
         await db.put_token(installation_id, token)
 
-    return RedirectResponse(f"https://github.com/settings/installations/{installation_id}")
+    return RedirectResponse(
+            f"https://github.com/settings/installations/{installation_id}")
+
 
 async def on_ping():
     pass
+
 
 async def on_pull_request(payload: Payload):
     if payload.action == 'closed':
@@ -166,7 +216,9 @@ async def on_pull_request(payload: Payload):
         headers = {
             'Accept': 'application/json',
         }
-        response = await client.post('https://github.com/login/oauth/access_token', data=data, headers=headers)
+        response = await client.post(TOKEN_ENDPOINT,
+                                     data=data,
+                                     headers=headers)
         if response.is_error:
             raise HTTPException(response.status_code, response.text)
         token = Token(**response.json())
@@ -174,14 +226,25 @@ async def on_pull_request(payload: Payload):
 
         files_response = await client.get(f'{payload.pull_request.url}/files')
         if files_response.is_error:
-            raise HTTPException(files_response.status_code, files_response.text)
+            raise HTTPException(files_response.status_code,
+                                files_response.text)
 
         files = files_response.json()
-        workflow_added = (f for f in files if f['filename'].startswith(".github/workflows/") and f['status'] == 'added')
+
+        def file_added(f):
+            return (f['filename'].startswith('.github/workflows')
+                    and f['status'] == 'added')
+
+        workflow_added = (f for f in files if file_added(f))
         if any(workflow_added):
             print("detected: workflow added")
 
-            url = f'{payload.pull_request.head.repo.url}/actions/runs?actor={payload.pull_request.user.login}&branch={payload.pull_request.head.ref}'
+            query = {
+                'actor': payload.pull_request.user.login,
+                'branch': payload.pull_request.head.ref,
+            }
+            url = payload.pull_request.head.repo.url
+            url = f'{url}/actions/runs?{urlencode(query)}'
             headers = {
                 'Authorization': f'Bearer {token.access_token}',
                 'Accept': 'application/vnd.github.v3+json',
@@ -189,31 +252,47 @@ async def on_pull_request(payload: Payload):
             workflows = await client.get(url, headers=headers)
             if workflows.is_error:
                 raise HTTPException(workflows.status_code, workflows.text)
-            urls = (x['url'] for x in workflows.json()['workflow_runs']) # FIXME
+            # FIXME filter status
+            urls = (x['url'] for x in workflows.json()['workflow_runs'])
             for url in urls:
                 response = await client.post(f'{url}/cancel', headers=headers)
                 if response.is_error:
                     print(response.status_code, response.text)
 
             comment = 'Sorry. Could not accept workflow added.'
-            comment_response = await client.post(f'{payload.pull_request.issue_url}/comments', headers=headers, json={'body': comment})
+            comment_response = await client.post(
+                    f'{payload.pull_request.issue_url}/comments',
+                    headers=headers,
+                    json={'body': comment})
             if comment_response.is_error:
-                raise HTTPException(comment_response.status_code, comment_response.text)
+                raise HTTPException(comment_response.status_code,
+                                    comment_response.text)
 
-            close_response = await client.patch(f'{payload.pull_request.url}', headers=headers, json={'state': 'closed'})
+            close_response = await client.patch(f'{payload.pull_request.url}',
+                                                headers=headers,
+                                                json={'state': 'closed'})
             if close_response.is_error:
-                raise HTTPException(close_response.status_code, close_response.text)
+                raise HTTPException(close_response.status_code,
+                                    close_response.text)
 
 
 async def on_installation():
     pass
 
+
 def verify_payload(payload: bytes, sig: str):
-    digest = hmac.new(WEBHOOK_SECRET.encode('utf8'), payload, 'sha256').hexdigest()
+    digest = hmac.new(WEBHOOK_SECRET.encode('utf8'),
+                      payload,
+                      'sha256').hexdigest()
     return hmac.compare_digest(sig, 'sha256=' + digest)
 
+
 @app.post("/webhook")
-async def post(request: Request, payload: Payload, x_gitHub_event: str = Header(None), x_hub_signature_256: str = Header(None)):
+async def post(request: Request,
+               payload: Payload,
+               x_gitHub_event: str = Header(None),
+               x_hub_signature_256: str = Header(None)):
+
     if not verify_payload(await request.body(), x_hub_signature_256):
         raise HTTPException(422, "signature not verified")
 
