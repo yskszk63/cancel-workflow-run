5552ecb22896bf48cb6ac06528bcfd9af30fb538
yskszk63 <yskszk63@gmail.com>
2021-04-17 11:51:40 +0900

    refactor1

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 import hmac
+import logging
 import os
 import time
-from typing import Any, Callable, Optional
+from typing import Any, Callable, Generator, Optional
 from urllib.parse import urlencode
 
 import jwt
@@ -15,6 +16,9 @@ from pydantic import BaseModel
 APP_ID = os.environ['APP_ID']
 WEBHOOK_SECRET = os.environb[b'WEBHOOK_SECRET']
 SECRET = os.environ['SECRET']
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
 class AppToken(BaseModel):
@@ -62,6 +66,23 @@ class Payload(BaseModel):
     sender: Optional[User]
 
 
+class PrFile(BaseModel):
+    filename: str
+    status: str
+
+    @property
+    def is_workflow(self: PrFile) -> bool:
+        return self.filename.startswith('.github/workflows')
+
+    @property
+    def is_added(self: PrFile) -> bool:
+        return self.status == 'added'
+
+
+class WorkflowRun(BaseModel):
+    url: str
+
+
 class VerifySignatureRoute(APIRoute):
     def get_route_handler(self: VerifySignatureRoute) -> Callable:
         original = super().get_route_handler()
@@ -82,6 +103,114 @@ class VerifySignatureRoute(APIRoute):
         return custom_route_handler
 
 
+async def get_token(client: AsyncClient, installation_id: int) -> AppToken:
+    now = int(time.time())
+
+    jwt_payload = {
+        'iat': now,
+        'exp': now + (60 * 5),
+        'iss': APP_ID,
+    }
+
+    jwt_token = jwt.encode(jwt_payload, SECRET, 'RS256')
+
+    headers = {
+        'Authorization': f'Bearer {jwt_token}',
+        'Accept': 'application/vnd.github.v3+json',
+    }
+
+    endpoint = 'https://api.github.com'
+    url = f'{endpoint}/app/installations/{installation_id}/access_tokens'
+
+    logger.debug(f'retrieve token from {url}')
+
+    response = await client.post(url, headers=headers)
+    if response.is_error:
+        raise Exception(f'failed to call: {response.status_code} {response.text}')
+
+    return AppToken(**response.json())
+
+
+async def call_as_api(client: AsyncClient,
+                      url: str,
+                      method: str,
+                      token: AppToken,
+                      *,
+                      json: Any = None) -> Any:
+    headers = {
+        'Authorization': f'Bearer {token.token}',
+        'Accept': 'application/vnd.github.v3+json',
+    }
+
+    logger.debug(f'call: {method} {url}')
+    response = await client.request(method, url, headers=headers, json=json)
+    if response.is_error:
+        raise Exception(f'failed to call: {response.status_code} {response.text}')
+
+    return response.json()
+
+
+async def list_pr_files(client: AsyncClient,
+                        token: AppToken,
+                        pull_request: PullRequest) -> list[PrFile]:
+
+    url = f'{pull_request.url}/files'
+    response = await call_as_api(client, url, 'get', token)
+    return [PrFile(**f) for f in response]
+
+
+async def comment_pr(client: AsyncClient,
+                     token: AppToken,
+                     pr: PullRequest,
+                     comment: str) -> None:
+
+    url = f'{pr.issue_url}/comments'
+    await call_as_api(client, url, 'post', token, json={'body': comment})
+
+
+async def close_pr(client: AsyncClient,
+                   token: AppToken,
+                   pr: PullRequest) -> None:
+
+    await call_as_api(client, pr.url, 'post', token, json={'state': 'closed'})
+
+
+async def list_workflow_runs(client: AsyncClient,
+                             token: AppToken,
+                             repo: Repository,
+                             pr: PullRequest) -> Generator[WorkflowRun,
+                                                           None,
+                                                           None]:
+
+    params = {
+        'actor': pr.user.login,
+        'branch': pr.head.ref,
+    }
+    url = f'{repo.url}/actions/runs?{urlencode(params)}'
+
+    while True:
+        json = dict(state='closed')
+        response = await call_as_api(client, url, 'get', token, json=json)
+
+        for run in response['workflow_runs']:
+            yield WorkflowRun(**run)
+
+        # FIXME
+        # https://docs.github.com/ja/rest/guides/traversing-with-pagination
+        break
+
+
+async def cancel_workflow(client: AsyncClient,
+                          token: AppToken,
+                          run: WorkflowRun) -> None:
+
+    url = f'{run.url}/cancel'
+    try:
+        await call_as_api(client, url, 'post', token)
+    except Exception as err:
+        logger.warn(err)
+
+
 app = FastAPI()
 app.router.route_class = VerifySignatureRoute
 
@@ -95,76 +224,29 @@ async def on_pull_request(payload: Payload) -> None:
         print('skip')
         return
 
-    now = int(time.time())
-    jwt_payload = {
-        'iat': now,
-        'exp': now + (60 * 5),
-        'iss': APP_ID,
-    }
-    jwt_token = jwt.encode(jwt_payload, SECRET, 'RS256')
+    pull_request = payload.pull_request
+    if pull_request is None:
+        raise HTTPException(400)
+
+    repository = payload.repository
+    if repository is None:
+        raise HTTPException(400)
 
     async with AsyncClient() as client:
-        headers = {
-            'Authorization': f'Bearer {jwt_token}',
-            'Accept': 'application/vnd.github.v3+json',
-        }
-        url = f'https://api.github.com/app/installations/{payload.installation.id}/access_tokens'
-        token_response = await client.post(url, headers=headers)
-        if token_response.is_error:
-            raise HTTPException(token_response.status_code,
-                                token_response.text)
-        token = AppToken(**token_response.json())
+        token = await get_token(client, payload.installation.id)
+        files = await list_pr_files(client, token, pull_request)
 
-        files_response = await client.get(f'{payload.pull_request.url}/files')
-        if files_response.is_error:
-            raise HTTPException(files_response.status_code,
-                                files_response.text)
-
-        files = files_response.json()
-
-        def file_added(f: dict[str, Any]) -> bool:
-            return (f['filename'].startswith('.github/workflows')
-                    and f['status'] == 'added')
-
-        workflow_added = (f for f in files if file_added(f))
-        if any(workflow_added):
+        workflow_added = [f for f in files if f.is_workflow and f.is_added]
+        if len(workflow_added):
             print('detected: workflow added')
 
-            query = {
-                'actor': payload.pull_request.user.login,
-                'branch': payload.pull_request.head.ref,
-            }
-            url = payload.repository.url
-            url = f'{url}/actions/runs?{urlencode(query)}'
-            headers = {
-                'Authorization': f'Bearer {token.token}',
-                'Accept': 'application/vnd.github.v3+json',
-            }
-            workflows = await client.get(url, headers=headers)
-            if workflows.is_error:
-                raise HTTPException(workflows.status_code, workflows.text)
-            # FIXME filter status
-            urls = (x['url'] for x in workflows.json()['workflow_runs'])
-            for url in urls:
-                response = await client.post(f'{url}/cancel', headers=headers)
-                if response.is_error:
-                    print(response.status_code, response.text)
+            runs = list_workflow_runs(client, token, repository, pull_request)
+            async for run in runs:
+                await cancel_workflow(client, token, run)
 
             comment = 'Sorry. Could not accept workflow added.'
-            comment_response = await client.post(
-                    f'{payload.pull_request.issue_url}/comments',
-                    headers=headers,
-                    json={'body': comment})
-            if comment_response.is_error:
-                raise HTTPException(comment_response.status_code,
-                                    comment_response.text)
-
-            close_response = await client.patch(f'{payload.pull_request.url}',
-                                                headers=headers,
-                                                json={'state': 'closed'})
-            if close_response.is_error:
-                raise HTTPException(close_response.status_code,
-                                    close_response.text)
+            await comment_pr(client, token, pull_request, comment)
+            await close_pr(client, token, pull_request)
 
 
 async def on_installation() -> None:
